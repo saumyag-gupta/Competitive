e2adf17edb222f93ccc782dbed6c21fe05a49639
Saumya Gupta <saumyag.gupta@gmail.com>
2018-07-07 16:49:17 +0530

    Update macros_codechef.cpp

--- macros_codechef.cpp
+++ macros_codechef.cpp
@@ -10,30 +10,34 @@ using namespace std;
 #define mll map<ll,ll>
 #define itr :: iterator
 #define pf push_front
-#define f first
-#define s second
+#define ff first
+#define ss second
+#define stkl stack <ll>
+#define quel queue <ll>
+#define pquel priority_queue <ll>
+#define rep(i,n) for(i=0;i<n;i++)
 #define MOD 1000000007
 #define PRIME 1000000007 // For mod inverse and binary exponentiation
 
 struct edge {
     ll A;
     ll B; 
-};       
- 
+};
+
 ll power(ll a, ll b, ll mod){
-	ll x = 1, y = a;
-	while (b > 0){
-		if (b%2){
-			x = (x*y)%mod;
-		}
-		y = (y*y)%mod;
-		b /= 2;
-	}
-	return x%mod;
-}	
+    ll x = 1, y = a;
+    while (b > 0){
+        if (b%2){
+            x = (x*y)%mod;
+        }
+        y = (y*y)%mod;
+        b /= 2;
+    }
+    return x%mod;
+}   
  
 ll modular_inverse(ll n, ll mod){
-	return power(n, mod-2, mod);
+    return power(n, mod-2, mod);
 }
 
 bool edgecompare(edge lhs, edge rhs) { if(lhs.B!=rhs.B)return lhs.B < rhs.B;
