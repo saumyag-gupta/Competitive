d2dcf95f7592b41f96bf6fac7fa5c31ddcf49023
saumyag-gupta <saumyag.gupta@gmail.com>
2018-07-07 16:48:29 +0530

    Update 7/7/2018

--- a2.cpp
+++ a2.cpp
@@ -10,43 +10,104 @@ using namespace std;
 #define mll map<ll,ll>
 #define itr :: iterator
 #define pf push_front
-#define f first
-#define s second
+#define ff first
+#define ss second
 #define MOD 1000000007
 
-int n,d,x[100];
-vector<int> ans;
+struct edge {
+    ll A;
+    ll B; 
+};
 
-int main(){
-    cin>>n>>d;
+bool edgecompare(edge lhs, edge rhs) { if(lhs.B!=rhs.B)return lhs.B < rhs.B;
+else return lhs.A<rhs.A;}
 
-    for(int i=0;i<n;i++) cin>>x[i];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    for(int i=0;i<n;i++){
-        int xl = x[i]-d;
-        int xr=x[i]+d;
+    ll n,m,i,j,k,flag=0,x,y,xx,yy;
+    vll conb,cone;
 
-        bool okayl=true;
-        bool okayr=true;
-
-        for(int j=0;j<n;j++){
-            if(fabs((ll)xl-x[j]) < (ll)d) okayl=false;
-            if(fabs((ll)xr-x[j]) < (ll)d) okayl=false;
-        }
-        if(okayl) ans.pb(xl);
-        if(okayr) ans.pb(xr);
-    }
-
-    sort(ans.begin(),ans.end());
-
-    int cnt=0;
-    int len=ans.size();
-
-    if(len==0)cout<<"0";
-    else
+    vl v,w;
+    
+    cin>>n>>xx>>yy;
+    
+    for(i=0;i<n;i++)
     {
-        cnt=1;
-        for(int i=1;i<len;i++) if(ans[i] !=ans[i-1])cnt++;
-        cout<<cnt;
+        cin>>x>>y;conb.pb({x,y});cone.pb({y,x});
     }
+
+    for(i=0;i<xx;i++)
+    {
+        cin>>x;v.pb(x);
+    }
+
+    for(i=0;i<yy;i++)
+    {
+        cin>>x;w.pb(x);
+    }
+
+    sort(conb.begin(),conb.end());
+    sort(cone.begin(),cone.end());
+    sort(v.begin(),v.end());
+    sort(w.begin(),w.end());
+
+    vll itr it;vl itr vit,wit;
+
+    vit=v.begin();
+    wit=w.begin();
+
+    mll time;
+
+    for(it=conb.begin();it!=conb.end();it++)
+    {
+        ll st=it->ff;
+        while((*vit)<=st)
+        {
+            vit++;
+            if(vit==v.end())break;
+        }
+        if(vit!=v.begin()){
+        vit--;
+        ll ans=1-(*vit);
+        time[st]=ans;
+        }   
+    }
+
+    for(it=cone.begin();it!=cone.end();it++)
+    {
+        ll en=it->ff;
+        ll st=it->ss;
+
+        while((*wit)<en && wit!=w.end())
+        {
+            wit++;
+            if(wit==w.end())break;
+        }
+
+        if(wit!=w.end() && time.find(st)!=time.end())
+        {
+            ll ans=*wit;
+            time[st]+=ans;
+        }
+        else if(time.find(st)!=time.end())
+        {
+            time[st]=INT_MAX;
+        }
+    }
+
+    mll itr mall;ll mi=INT_MAX;
+
+    for(mall=time.begin();mall!=time.end();mall++)
+    {
+        if(mall->ss < mi)mi=mall->ss;
+
+    }
+
+    cout<<mi;
+
+   return 0;
 }
--- trie.cpp
+++ trie.cpp
@@ -10,35 +10,14 @@ using namespace std;
 #define mll map<ll,ll>
 #define itr :: iterator
 #define pf push_front
-#define ff first
-#define ss second
-#define stkl stack <ll>
-#define quel queue <ll>
-#define pquel priority_queue <ll>
-#define rep(i,n) for(i=0;i<n;i++)
+#define f first
+#define s second
 #define MOD 1000000007
-#define PRIME 1000000007 // For mod inverse and binary exponentiation
 
 struct edge {
     ll A;
     ll B; 
 };
-
-ll power(ll a, ll b, ll mod){
-    ll x = 1, y = a;
-    while (b > 0){
-        if (b%2){
-            x = (x*y)%mod;
-        }
-        y = (y*y)%mod;
-        b /= 2;
-    }
-    return x%mod;
-}   
- 
-ll modular_inverse(ll n, ll mod){
-    return power(n, mod-2, mod);
-}
 
 bool edgecompare(edge lhs, edge rhs) { if(lhs.B!=rhs.B)return lhs.B < rhs.B;
 else return lhs.A<rhs.A;}
@@ -48,10 +27,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll t;
-    cin>>t;
-    while(t--)
-{
+
     ll n,m,i,j,k,flag=0,x;
     vl a;
     
@@ -62,7 +38,5 @@ int main()
         cin>>x;a.pb(x);
     }
 
-
-}
    return 0;
 }
